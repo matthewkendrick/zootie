47cff7d3c97b5c4649a47626c7e4bbeb95c46bae
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-11-14 12:49:13 +0900

    feat: スタイリングコンテンツを完成させた
    Message body
    以下を着手していく
    ・メインのコンテンツを追加する
    
    Message footer

--- js/styling.js
+++ js/styling.js
@@ -1,10 +1,10 @@
 const images = [
-  "https://data.whicdn.com/images/251238216/original.gif",
-  "https://i.pinimg.com/originals/3d/22/45/3d224511a5e13317e46e37bee1d249dd.gif",
-  "https://i.pinimg.com/originals/b0/10/e9/b010e954f94acbd034917b2d6931bd79.gif",
-  "https://64.media.tumblr.com/5f4c0252b15dda55028536c5a8923c7d/b691a90722d7bbb5-c8/s500x750/34aac55dfd7302e41fec400ba9636edeadb1890a.gif",
-  "https://64.media.tumblr.com/746e848c8b0cf90bc7938599421e6b4e/tumblr_pbhfk0rEth1txe8seo1_500.gif",
-  "https://media2.giphy.com/media/Wm9XlKG2xIMiVcH4CP/giphy.gif"
+  "https://thumbnail.image.rakuten.co.jp/@0_mall/e-zakkamania/cabinet/coordinate/2211/22111010.jpg?_ex=750x750",
+  "https://thumbnail.image.rakuten.co.jp/@0_mall/e-zakkamania/cabinet/coordinate/2211/22110921.jpg?_ex=750x750",
+  "https://thumbnail.image.rakuten.co.jp/@0_mall/e-zakkamania/cabinet/coordinate/2211/22110918.jpg?_ex=750x750",
+  "https://thumbnail.image.rakuten.co.jp/@0_mall/e-zakkamania/cabinet/coordinate/2211/22110917.jpg?_ex=750x750",
+  "https://thumbnail.image.rakuten.co.jp/@0_mall/e-zakkamania/cabinet/coordinate/2211/22110911.jpg?_ex=500x500&s=2&r=1",
+  "https://thumbnail.image.rakuten.co.jp/@0_mall/e-zakkamania/cabinet/coordinate/2211/22110912.jpg?_ex=500x500&s=2&r=1"
 ];
 
 $(document).ready(function () {
